bc9e96c59bb76e8e9b177ca4eb55a64982d5e939
Stephan <cicchelli.stephan@gmail.com>
2024-05-03 13:04:18 -0300

    req 3

--- news/validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def validate_category_name(value):
-    if len(value) == 0 or len(value) < 200:
-        raise ValidationError("Invalid category name.")
