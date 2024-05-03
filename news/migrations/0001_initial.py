9638f706490a20f75aa797c024fe3fd11636223f
Stephan <cicchelli.stephan@gmail.com>
2024-05-03 12:58:39 -0300

    req 3

--- news/migrations/0001_initial.py
+++ news/migrations/0001_initial.py
@@ -1,12 +1,11 @@
 from django.db import migrations, models
-import news.validators
+import django.db.models.deletion
+import news.models
 
 
 class Migration(migrations.Migration):
     initial = True
-
     dependencies = []
-
     operations = [
         migrations.CreateModel(
             name="Category",
@@ -20,11 +19,62 @@ class Migration(migrations.Migration):
                         verbose_name="ID",
                     ),
                 ),
+                ("name", models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="User",
+            fields=[
                 (
-                    "name",
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=200)),
+                ("email", models.EmailField(max_length=200)),
+                ("password", models.CharField(max_length=200)),
+                ("role", models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
+            name="News",
+            fields=[
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "title",
                     models.CharField(
-                        max_length=200,
-                        validators=[news.validators.validate_category_name],
+                        max_length=200, validators=[news.models.validate_title]
+                    ),
+                ),
+                ("content", models.TextField()),
+                ("created_at", models.DateField()),
+                (
+                    "img",
+                    models.ImageField(blank=True, null=True, upload_to="img/"),
+                ),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="news.user",
+                    ),
+                ),
+                (
+                    "categories",
+                    models.ManyToManyField(
+                        related_name="news", to="news.category"
                     ),
                 ),
             ],
--- news/models.py
+++ news/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-from news.validators import validate_category_name
+from django.core.exceptions import ValidationError
 
 
 class Category(models.Model):
-    name = models.CharField(
-        max_length=200, validators=[validate_category_name]
-    )
+    name = models.CharField(max_length=200, blank=False, null=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
@@ -19,3 +17,25 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+def validate_title(value):
+    words = value.split()
+    if len(words) <= 1:
+        raise ValidationError("O título deve ter mais de uma palavra.")
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200, validators=[validate_title], blank=False, null=False
+    )
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey("User", on_delete=models.CASCADE)
+    created_at = models.DateField(blank=False, null=False)
+    img = models.ImageField(upload_to="img/", blank=True, null=True)
+    categories = models.ManyToManyField(
+        Category, related_name="news", blank=False
+    )
+
+    def __str__(self) -> str:
+        return self.title
