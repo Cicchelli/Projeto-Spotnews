7c2a283d32d8ddc8fda20086eff58435c011efc1
Stephan <cicchelli.stephan@gmail.com>
2024-05-03 15:50:52 -0300

    req 6

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from news.views import index, news_details
+
+from news.views import index, news_details, categories_form
 
 
 urlpatterns = [
@@ -8,5 +9,10 @@ urlpatterns = [
         "http://127.0.0.1:8000/news/<int:id>",
         news_details,
         name="news-details-page",
+    ),
+    path(
+        "http://127.0.0.1:8000/categories/",
+        categories_form,
+        name="categories-form",
     ),
 ]
--- news/views.py
+++ news/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from news.models import Category, News
+from news.forms import CreateCategoriesForm
 
-from news.models import News
 
-
-# Create your views here.
 def index(request):
-    context = {"news": News.objects.all()}
-    return render(request, "home.html", context)
+    news = News.objects.all()
+    return render(request, "home.html", {"news": news})
 
 
 def news_details(request, id):
-    context = {"news_details": get_object_or_404(News, id=id)}
-    return render(request, "news_details.html", context)
+    news_details = get_object_or_404(News, id=id)
+    return render(request, "news_details.html", {"news_details": news_details})
+
+
+def categories_form(request):
+    form = CreateCategoriesForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        Category.objects.create(**form.cleaned_data)
+        return redirect("home-page")
+    return render(request, "categories_form.html", {"form": form})
